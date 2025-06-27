29885037b4b75faac616de4ea2fdad91ab2d704c
jumainfomagnus <163571282+jumainfomagnus@users.noreply.github.com>
2025-06-27 18:09:34 -0400

    update the code to deploy from gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/DenverDevDay_Demo/', // Updated with correct repository name
   server: {
     port: 3000,
     open: true
